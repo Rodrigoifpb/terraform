c58b5864d82c57781e6fdeba3afb711d17fdc1d5
rodrigoifpb <rodrigoflafla@gmail.com>
2021-02-01 21:29:53 -0300

    estudando vars

---
+++ variaveis/variables.tf
@@ -0,0 +1,25 @@
+variable "region" {
+  type        = string
+  default     = "us-east-1"
+  description = "regiao padrao"
+}
+
+variable "acces_key" {
+  type        = string
+  default     = "value"
+}
+
+variable "secret_key" {
+  type        = string
+  default     = "value"
+}
+
+variable "ami" {
+  type        = string
+  default     = ""
+  description = "linux basico"
+}
+
+variable "type" {
+  
+}
