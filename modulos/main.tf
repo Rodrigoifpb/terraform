7d500bcd8ebc051db64497042d043d2a6946563e
rodrigoifpb <rodrigoflafla@gmail.com>
2021-02-02 22:31:46 -0300

    adiconando mais arquivos de modulos

--- modulos/main.tf
+++ modulos/main.tf
@@ -10,4 +10,12 @@ module "bucket" {
     source = "./s3"
 
     name = "meu-bucket-${random_id.bucket.hex}"
+
+    tags = {
+        "Name" = "arquivos diversos"
+    }
+
+    create_object = true
+    object_key = "files/${random_id.bucket.dec}.txt"
+    object_source = "file.txt"
 }
